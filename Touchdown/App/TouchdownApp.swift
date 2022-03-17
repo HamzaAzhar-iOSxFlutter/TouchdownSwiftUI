5fea001e6cea5ed90025cd7eb423c623b16b9e36
hamzaazharxiOS <hamza.azhar@avancerasolution.com>
2022-03-17 15:59:59 +0500

    initial commit

---
+++ Touchdown/App/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Hamza Azhar on 17/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
